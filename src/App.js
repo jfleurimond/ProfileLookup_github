9e71b5b8fd4fdd9bdf937aac1d6a89f11ad539cb
Justin Fleurimond <jfleurimond24@gmail.com>
2022-02-13 17:51:25 -0500

    refactor code Updated my code to make it more cleaner and not wrap the useEffect within an if condition. Since it is not allowed within React

--- src/App.js
+++ src/App.js
@@ -4,24 +4,39 @@ import { useState, useEffect } from "react";
 export default function App() {
   const [users, setUsers] = useState([]);
 
-  if (localStorage.getItem("test") === null) {
-    useEffect(() => {
-      fetch("https://api.github.com/users/jfleurimond")
-        .then((res) => res.json())
-        .then(setUsers)
-        .catch((err) => console.log(err));
-    }, []);
-  } else {
+  const [loadData, setLoadData] = useState(false);
+
+  // if (localStorage.getItem("test") === null) {
+  //   setLoadData(true);
+  // } else {
+  //   setUsers(JSON.parse(localStorage.getItem("test")));
+  // }
+
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/jfleurimond")
+  //     .then((res) => res.json())
+  //     .then(setUsers)
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/jfleurimond")
+      .then((res) => res.json())
+      .then((data) => localStorage.setItem("test", JSON.stringify(data)))
+      .catch((err) => console.log(err));
+
     setUsers(JSON.parse(localStorage.getItem("test")));
-  }
+  }, []);
+
+  console.log(users);
   // future note to add else condition to
   // setUsers with localstorage.getItem
 
-  useEffect(() => {
-    const usersString = JSON.stringify(users);
-    localStorage.setItem("test", usersString);
-    console.log(localStorage.getItem("test"));
-  }, [users]);
+  // useEffect(() => {
+  //   const usersString = JSON.stringify(users);
+  //   localStorage.setItem("test", usersString);
+  //   console.log(localStorage.getItem("test"));
+  // }, [users]);
 
   return <p> test {users.login} </p>;
 }
