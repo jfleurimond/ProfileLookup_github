1692b6a3483bf5f2fc64c2c9d8965f6fb268473c
Justin Fleurimond <jfleurimond24@gmail.com>
2022-02-13 17:54:51 -0500

    deleted unnecessary comments deleted unnecessary comments

--- src/App.js
+++ src/App.js
@@ -4,21 +4,6 @@ import { useState, useEffect } from "react";
 export default function App() {
   const [users, setUsers] = useState([]);
 
-  const [loadData, setLoadData] = useState(false);
-
-  // if (localStorage.getItem("test") === null) {
-  //   setLoadData(true);
-  // } else {
-  //   setUsers(JSON.parse(localStorage.getItem("test")));
-  // }
-
-  // useEffect(() => {
-  //   fetch("https://api.github.com/users/jfleurimond")
-  //     .then((res) => res.json())
-  //     .then(setUsers)
-  //     .catch((err) => console.log(err));
-  // }, []);
-
   useEffect(() => {
     fetch("https://api.github.com/users/jfleurimond")
       .then((res) => res.json())
@@ -29,14 +14,12 @@ export default function App() {
   }, []);
 
   console.log(users);
-  // future note to add else condition to
-  // setUsers with localstorage.getItem
 
-  // useEffect(() => {
-  //   const usersString = JSON.stringify(users);
-  //   localStorage.setItem("test", usersString);
-  //   console.log(localStorage.getItem("test"));
-  // }, [users]);
+  useEffect(() => {
+    const usersString = JSON.stringify(users);
+    localStorage.setItem("test", usersString);
+    console.log(localStorage.getItem("test"));
+  }, [users]);
 
   return <p> test {users.login} </p>;
 }
