d8a39e3afec7d20d74f7fc6ba6849cfda7ab141a
Justin Fleurimond <jfleurimond24@gmail.com>
2022-02-13 14:58:20 -0500

    added else condition when localstorage null Added else condition to set values in usestae, if localsorage is not null

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { useState, useEffect } from "react";
 export default function App() {
   const [users, setUsers] = useState([]);
 
-  if (localStorage.getItem("test") != null) {
+  if (localStorage.getItem("test") === null) {
     useEffect(() => {
       fetch("https://api.github.com/users/jfleurimond")
         .then((res) => res.json())
         .then(setUsers)
         .catch((err) => console.log(err));
     }, []);
+  } else {
+    setUsers(JSON.parse(localStorage.getItem("test")));
   }
-
   // future note to add else condition to
   // setUsers with localstorage.getItem
 
